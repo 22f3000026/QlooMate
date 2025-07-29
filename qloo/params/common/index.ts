1ea57b3b491022b2fef52a18379b450e4ea0faa6
akdeepankar <akdeepaknyc@gmail.com>
2025-07-30 01:14:21 +0530

    final

--- qloo/params/common/index.ts
+++
@@ -1,5 +0,0 @@
-import { QlooFilterParams } from "./filter-params.ts";
-import { QlooSignalParams } from "./signal-params.ts";
-import { QlooOutputParams } from "./output-params.ts";
-
-export type QlooParams = QlooFilterParams & QlooSignalParams & QlooOutputParams;
